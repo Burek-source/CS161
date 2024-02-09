470939c932f19872adf1f59df846bd905ce00358
Rudolf Sose <rudolf.sose@hotmail.com>
2024-02-09 12:30:38 +0000

    Week 1

---
+++ whatPrints.java
@@ -0,0 +1,25 @@
+/*
+
+What is the Output
+
+What do the below statements print?
+
+Write a Java program that contains all of the statements to find out. 
+
+Sample code
+
+System.out.println("5 + 6");
+System.out.println(5 + 6);
+System.out.println(5 + "6");
+
+ */
+
+public class whatPrints 
+{
+    public static void main (String[] args)
+    {
+        System.out.println("5 + 6");
+        System.out.println(5 + 6);
+        System.out.println(5 + "6");
+    }
+}
