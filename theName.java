b6a8016a02448abe6781a62dd5f7623244a08688
Rudolf Sose <rudolf.sose@hotmail.com>
2024-02-09 12:25:02 +0000

    Week 1

---
+++ theName.java
@@ -0,0 +1,21 @@
+/*  
+
+Write a Java program that prints the name "John Doe" three times. Twice on
+line 1, and once on line 2.
+
+Sample Output
+
+John Doe John Doe
+John Doe
+
+*/
+
+
+public class theName 
+{
+    public static void main (String[] args)
+    {
+        System.out.println("John Doe John Doe");
+        System.out.println("John Doe");
+    }
+}
