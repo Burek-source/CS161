59b080c9c31053859348c2d6a9b9c30450e68cbb
Rudolf Sose <rudolf.sose@hotmail.com>
2024-02-09 12:37:07 +0000

    Week 1

---
+++ creatingVariables.java
@@ -0,0 +1,40 @@
+/*
+
+Creating Variables
+
+Write a Java program that:
+
+1. Declares an integer, called num1, and sets its value to be 12.
+2. Declares a double, called num2, and sets its value to be 123.4.
+3. Declares a character variable, called letter, and sets its value to be 'c'.
+4. Prints each initialised variable on a new line.
+
+You must not use print statements to print out 12, 123.4 or c, but use them 
+to print out the variables using their names.
+
+NOTE: You must use the variable names and values given.
+
+Sample output
+
+12
+123.4
+c
+
+ */
+
+public class creatingVariables 
+{
+
+    public static void main (String[] args)
+    {
+        int num1 = 12;
+        double num2 = 123.4;
+        char letter = 'c';
+
+        System.out.println(num1);
+        System.out.println(num2);
+        System.out.println(letter);
+
+    }
+    
+}
